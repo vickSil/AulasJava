a57446019297fd5c0314b4548782a752cb625734
aluno <aluno@DSN1251230714>
2022-04-19 17:28:04 -0300

    gets e sets

--- ProjetoSomaDe3Numeros/src/main/java/br/com/senai/vitoriasilva/application/PageBean.java
+++ ProjetoSomaDe3Numeros/src/main/java/br/com/senai/vitoriasilva/application/PageBean.java
@@ -15,24 +15,37 @@ public class PageBean implements Serializable {
 	 private int n2;
 	 private int n3;
 	 
-	 @Inject
-	 private Flash flash;
+	public int getN1() {
+		return n1;
+	}
+	public void setN1(int n1) {
+		this.n1 = n1;
+	}
+	public int getN2() {
+		return n2;
+	}
+	public void setN2(int n2) {
+		this.n2 = n2;
+	}
+	public int getN3() {
+		return n3;
+	}
+	public void setN3(int n3) {
+		this.n3 = n3;
+	}
 	 
-	 public String processar() {
-		 
-		 flash.put("nomeDoUsuario", nome);
-		 
-		 return "result?faces-redirect=true";
-	 }
+//	 @Inject
+//	 private Flash flash;
+//	 
+//	 public String processar() {
+//		 
+//		 flash.put("nomeDoUsuario", nome);
+//		 
+//		 return "result?faces-redirect=true";
+//	 }
 	 
 
-	public String getNome() {
-		return nome;
-	}
 
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-	
+	 
 
 }
