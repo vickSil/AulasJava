2b8ef8643f8b0d5b024f001b472c465b96d25406
aluno <aluno@DSN1251230714>
2022-04-19 17:24:28 -0300

    Projeto JSF de Soma de 3 Números

---
+++ ProjetoSomaDe3Numeros/src/main/java/br/com/senai/vitoriasilva/application/PageBean.java
@@ -0,0 +1,38 @@
+ package br.com.senai.vitoriasilva.application;
+
+import java.io.Serializable;
+
+import javax.enterprise.context.RequestScoped;
+import javax.faces.context.Flash;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+@Named
+@RequestScoped
+public class PageBean implements Serializable {
+	
+	 private int n1;
+	 private int n2;
+	 private int n3;
+	 
+	 @Inject
+	 private Flash flash;
+	 
+	 public String processar() {
+		 
+		 flash.put("nomeDoUsuario", nome);
+		 
+		 return "result?faces-redirect=true";
+	 }
+	 
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+
+}
