112d110c7a020a5949e44eb95f0a415ecce58daa
aluno <aluno@DSN1251230714>
2022-04-19 15:33:39 -0300

    Projeto Tela de Navegação 2

---
+++ ProjetoNavegacaoTela1/src/main/java/br/com/senai/vitoriasilva/application/PageBean.java
@@ -0,0 +1,40 @@
+package br.com.senai.vitoriasilva.application;
+
+import java.io.Serializable;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+
+@SuppressWarnings("serial")
+@Named
+@RequestScoped
+
+public class PageBean implements Serializable {
+
+	private String page;
+
+	public String getPage() {
+		return page;
+	}
+
+	public void setPage(String page) {
+		this.page = page;
+	}
+	
+	public enum Pages{
+		page_a, page_b;
+	}
+	
+	public Pages goTo() {
+
+		if("a".equals(page)) {
+			return Pages.page_a;
+		}else if("b".equals(page)) {
+			return Pages.page_b;
+		}else {
+			return null;
+		}
+		
+	}
+		
+}
